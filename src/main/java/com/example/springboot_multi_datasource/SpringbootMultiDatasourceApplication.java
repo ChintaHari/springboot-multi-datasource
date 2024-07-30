967a56c252761fceb06e2b8c405415a6e98c7359
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-07-29 20:48:24 -0500

    Initial Commit

---
+++ src/main/java/com/example/springboot_multi_datasource/SpringbootMultiDatasourceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_multi_datasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMultiDatasourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMultiDatasourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot_multi_datasource/SpringbootMultiDatasourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_multi_datasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMultiDatasourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
